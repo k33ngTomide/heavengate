d1607ea3260a88e4bfcfe4a4c715859c60aebec7
k33ngTomide <Msodiqakintomide@gmail.com>
2024-04-25 05:51:41 +0100

    Added book of life

--- src/App.js
+++ src/App.js
@@ -1,30 +1,86 @@
 import logo from './assets/gate.jpg';
 import './App.css';
+import { Avatar, Button, Card, CardContent, Grid, Typography } from '@mui/material';
+import { useState } from 'react';
 
 function App() {
+  const [users, setUsers] = useState([])
+
+  const getUsers = () => {
+    fetch('https//localhost:8081/getUsers')
+    .then(res => res.json())
+    .then(res => setUsers(res))
+    .catch(error => console.log(error))
+  }
+
   return (
     <div className="App">
 
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <Typography variant='h1'>
           Welcome to Heaven's gate
+        </Typography>
 
-          Click on the door to open
-        </p>
-
-        <p>Ahbi you no want enter heaven</p>
-        <a
-          className="App-link"
-          href="https://google.com"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Button
+          variant='filled'
+          onClick={getUsers}
         >
-          Go to google
-        </a>
+          Open Book of Life
+        </Button>
+
+        <Grid container spacing={2}>
+          {users.map(user => (
+            <BookCard key={user.id} user={user} />
+          ))}
+        </Grid>
       </header>
     </div>
   );
+}
+
+const BookCard = (user) => {
+
+  return(
+    <Card variant="outlined">
+      <CardContent>
+        <Grid container spacing={2} alignItems="center">
+          <Grid item>
+            <Avatar alt={user.name} src={user.image} />
+          </Grid>
+          <Grid item>
+            <Typography variant="h5" component="div">
+              {user.firstName} {user.lastName}
+            </Typography>
+            <Typography variant="body2">
+              Email: {user.email}
+            </Typography>
+            <Typography variant="body2">
+              Username: {user.username}
+            </Typography>
+            <Typography variant="body2">
+              Age: {user.age}
+            </Typography>
+            <Typography variant="body2">
+              Gender: {user.gender}
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant="body1" color="textSecondary">
+              Blood Group: {user.bloodGroup}
+            </Typography>
+            <Typography variant="body1" color="textSecondary">
+              Height: {user.height} cm
+            </Typography>
+            <Typography variant="body1" color="textSecondary">
+              Department: {user.department}
+            </Typography>
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+  )
+
 }
 
 export default App;
